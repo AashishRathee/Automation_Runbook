518c0ce3c08ab279f9c112b8f6c690a05324eac6
AashishRathee <100285985+AashishRathee@users.noreply.github.com>
2022-09-26 14:51:52 +0530

    Runbook updated for Automation account

--- Runbook to create VM.ps1
+++ Runbook to create VM.ps1
@@ -5,13 +5,13 @@ $AzureContext = (Connect-AzAccount -Identity).context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
 
 #defining variables
-$RG_Name = "Powershell-grp"
-$VM_Name = "Powershellvm"
-$location = "North Europe"
-$vnet = "Powershell-vnet"
-$subnet = "Subnet-Test1"
-$Publicip = "Powershell-IP"
-$NSG = "Powershell-nsg"
+$RG_Name = "Automation-grp"
+$VM_Name = "Automationvm"
+$location = "Central India"
+$vnet = "Automation-vnet"
+$subnet = "Subnet-Auto"
+$Publicip = "Automation-IP"
+$NSG = "Automation-nsg"
 $VM_Image = "Win2019DataCenter"
 
 New-AzVM -Name $VM_Name -ResourceGroupName $RG_Name -Location $location -VirtualNetworkName $vnet -SubnetName $subnet -PublicIpAddressName $Publicip -SecurityGroupName $NSG -Image $VM_Image
